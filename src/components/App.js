5e8ab6a58366e84f29dd031b950fa1a35c1d1cfa
felix manibo <felixcsm@gmail.com>
2019-02-13 23:29:59 +0800

    addind detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
     return (
-        <div>
-            <SongList />
+        <div className="ui container grid">
+            <div className="ui grid row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
         </div>
     );
 };
